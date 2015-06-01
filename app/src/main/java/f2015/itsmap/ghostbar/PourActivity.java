5d3e1713db4fe880b74ec5aada525915367b2f45
AMMP <amm.petersen@hotmail.com>
2015-06-01 14:41:46 +0200

    HTTP post WIP

--- app/src/main/java/f2015/itsmap/ghostbar/PourActivity.java
+++ app/src/main/java/f2015/itsmap/ghostbar/PourActivity.java
@@ -3,6 +3,7 @@ package f2015.itsmap.ghostbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,14 +12,28 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.InputStream;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import dk.danskebank.mobilepay.sdk.MobilePay;
 import dk.danskebank.mobilepay.sdk.ResultCallback;
 import dk.danskebank.mobilepay.sdk.model.FailureResult;
 import dk.danskebank.mobilepay.sdk.model.Payment;
 import dk.danskebank.mobilepay.sdk.model.SuccessResult;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.json.*;
 
 
 public class PourActivity extends AppCompatActivity {
@@ -111,6 +126,49 @@ public class PourActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(SuccessResult result) {
                     Toast.makeText(getApplicationContext(), "Payment succes", Toast.LENGTH_SHORT).show();
+
+                    /*JSONObject jsonObj;
+
+                    try {
+                        jsonObj = new JSONObject("{\"transactionId\":\"1\",\"price\":\"2.5\",\"amount\":\"150\"}");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }*/
+
+                    String jsonString = "{\"transactionId\":\"1\",\"price\":\"2.5\",\"amount\":\"150\"}";
+                    HttpClient client = new DefaultHttpClient();
+                    HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+                    HttpResponse response;
+                    String URL = "http://2party.dk/itsmap/beerbot_post.php";
+
+                    try{
+                        HttpPost post = new HttpPost(URL);
+
+                        // Create a NameValuePair out of the JSONObject + a name
+                        //List<NameValuePair> nVP = new ArrayList<NameValuePair>(2);
+                        //nVP.add(new BasicNameValuePair("json", json.toString()));
+
+                        // Hand the NVP to the POST
+                        if(jsonString !=null) {
+                            post.setEntity(new StringEntity(jsonString));
+                            Log.i("main", "TestPOST - nVP = " + jsonString.toString());
+                        }
+
+                        // Collect the response
+                        response = client.execute(post);
+
+            /*Checking response */
+                        if(response!=null){
+                            InputStream in = response.getEntity().getContent(); //Get the data in the entity
+                            int stuff = in.read();
+                            Toast.makeText(getApplicationContext(),stuff, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                        //createDialog("Error", "Cannot Establish Connection");
+                    }
+
                 }
                 @Override
                 public void onFailure(FailureResult result) {
