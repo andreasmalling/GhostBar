cf9cd27789aff719945e3b41f0af88fed8a48fd3
AMMP <amm.petersen@hotmail.com>
2015-06-03 15:32:37 +0200

    Removed redundant try/catch

--- app/src/main/java/f2015/itsmap/ghostbar/HTTPHandler.java
+++ app/src/main/java/f2015/itsmap/ghostbar/HTTPHandler.java
@@ -24,11 +24,12 @@ public class HTTPHandler extends AsyncTask<String, Void, String> {
 
     private Context context;
     String line;
-    HTTPHandler(Context c)
-    {
+
+    HTTPHandler(Context c) {
         context = c;
     }
-    protected String doInBackground(String... URL){
+
+    protected String doInBackground(String... URL) {
         String returnString = "";
         HttpClient client = new DefaultHttpClient();
         HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
@@ -37,7 +38,7 @@ public class HTTPHandler extends AsyncTask<String, Void, String> {
 
         String jsonString = "{\"transactionId\":\"1\",\"price\":\"2.5\",\"amount\":\"150\"}";
 
-        try{
+        try {
             HttpPost post = new HttpPost(URL[0]);
 
             // Create a NameValuePair out of the JSONObject + a name
@@ -49,32 +50,26 @@ public class HTTPHandler extends AsyncTask<String, Void, String> {
             post.setEntity(new StringEntity(jsonString));
             Log.i("main", "TestPOST - nVP = " + jsonString);
 
-            try {
-
-                // Collect the response
-                response = client.execute(post);
+            // Collect the response
+            response = client.execute(post);
 
             /*Checking response */
-                if (response != null) {
-                    InputStream in = response.getEntity().getContent(); //Get the data in the entity
-                    BufferedReader r = new BufferedReader(new InputStreamReader(in));
-                    StringBuilder total = new StringBuilder();
-                    //line = "";
+            if (response != null) {
+                InputStream in = response.getEntity().getContent(); //Get the data in the entity
+                BufferedReader r = new BufferedReader(new InputStreamReader(in));
+                StringBuilder total = new StringBuilder();
+                //line = "";
 
-                    while ((line = r.readLine()) != null) {
-                        total.append(line);
-                        //Toast.makeText(context, line, Toast.LENGTH_SHORT).show();
-                    }
-                    returnString = total.toString();
-
-                    //Toast.makeText(context, line, Toast.LENGTH_LONG).show();
+                while ((line = r.readLine()) != null) {
+                    total.append(line);
+                    //Toast.makeText(context, line, Toast.LENGTH_SHORT).show();
                 }
+                returnString = total.toString();
+
+                //Toast.makeText(context, line, Toast.LENGTH_LONG).show();
             }
-            catch (Exception e) {
-                //Toast.makeText(context, e.toString(),Toast.LENGTH_LONG).show();
-            }
-        }
-        catch(Exception e){
+        } 
+        catch (Exception e) {
             e.printStackTrace();
             //createDialog("Error", "Cannot Establish Connection");
         }
@@ -87,7 +82,7 @@ public class HTTPHandler extends AsyncTask<String, Void, String> {
         // TODO: do something with the feed
         //Toast.makeText(context, line, Toast.LENGTH_SHORT).show();
         String[] splitString = s.split(":");
-        String BeerId = splitString[3].replaceAll("\\p{P}","");
+        String BeerId = splitString[3].replaceAll("\\p{P}", "");
         Toast.makeText(context, "Your beer ID is: " + BeerId, Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(context, SuccesActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
